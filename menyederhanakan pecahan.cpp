7c6ed585f828432635f3a3256b3b78eb1dff5802
ItaLestari11 <45660967+ItaLestari11@users.noreply.github.com>
2018-12-15 20:13:15 +0700

    Add files via upload

---
+++ menyederhanakan pecahan.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int pecahan();
+int main()
+{
+    pecahan();
+}
+int pecahan()
+{
+    int a,b,l,t;
+    printf("Masukan Pembilang :");
+    scanf("%d", &a);
+    printf("Masukan Penyebut:");
+    scanf("%d", &b);
+    t=a/b;
+    l=a%b;
+    if (a%b ==0)
+        printf("Bilangan (%d/%d)\n di sederhanakan menjadi %d", a,b,t);
+    else if (a%b !=0)
+        printf("Bilangan (%d/%d\n di sederhanakan menjadi (%d %d/%d)",a,b,t,l,b);
+}
